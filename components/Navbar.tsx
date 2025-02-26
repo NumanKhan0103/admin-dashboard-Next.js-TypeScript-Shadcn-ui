3c046a4028a6c72cfcef8b3caf3286e462c8540f
Dev Nome <numanb@code-x.co>
2025-02-26 14:55:16 +0500

    integrated light and dark theme button with functionility

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,35 +10,44 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
+import ThemeToggler from '@/components/ThemeToggler';
+
 
 const Navbar = () => {
     return (
-        <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
-            <Link href="/">
-                <Image src="/logo.png" alt="TraversyPress" width={40} height={40} />
-            </Link>
+        <div className="border-b">
+            <div className="flex h-16 items-center px-4">
+                <Link href="/" className="flex items-center">
+                    <div className="relative h-10 w-10 mr-2">
+                        <div className="bg-primary text-primary-foreground rounded-lg p-2 h-full w-full flex items-center justify-center font-bold text-xl">
+                            AD
+                        </div>
+                    </div>
+                    <span className="hidden md:block font-bold">Admin Dashboard</span>
+                </Link>
 
-
-
-            <DropdownMenu>
-                <DropdownMenuTrigger>
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                        <AvatarFallback className="text-black">BT</AvatarFallback>
-                    </Avatar>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                        <Link href="/profile">Profile</Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <Link href="/auth">Logout</Link>
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-
+                <div className="ml-auto flex items-center space-x-4">
+                    <ThemeToggler />
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                <AvatarFallback>AD</AvatarFallback>
+                            </Avatar>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>
+                                <Link href="/profile">Profile</Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link href="/auth">Logout</Link>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            </div>
         </div>
     );
 };
