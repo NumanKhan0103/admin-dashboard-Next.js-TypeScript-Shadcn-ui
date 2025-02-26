d08995d1d561655f23d88fb80ae934ffa5107702
Dev Nome <numanb@code-x.co>
2025-02-26 15:39:44 +0500

    create separate layout for auth and main

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
+
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider"
 const geistSans = Geist({
@@ -36,18 +35,11 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-        <Navbar />
+      
+        { children}
 
-        <div className="flex">
-          <div className="hidden md:block h-[100vh] w-[350px]">
-            <Sidebar />
-          </div>
-          <div className="p-5 w-full md:max-w-[1140px]">
-            {children}
-          </div>
-          {/* toaster for show message  */}
-          <Toaster />
-        </div>
+         {/* toaster for show message  */}
+         <Toaster />
         </ThemeProvider>
       </body>
     </html>
